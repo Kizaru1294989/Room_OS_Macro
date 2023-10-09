7537fbcede92c72fc834011895140bbb54123c65
Ryan RAIS <rrais@exaprobe.com>
2023-10-09 10:34:29 +0200

    init

---
+++ push.sh
@@ -0,0 +1,16 @@
+echo "Enter your message"
+read message
+echo "Processing"
+git add .
+git commit -m"${message}"
+git status
+echo "Pushing data to https://github.com/Kizaru1294989/Room_OS_Macro.git"
+git push
+echo "
+███████ ██    ██  ██████  ██████ ███████ ███████ ███████ 
+██      ██    ██ ██      ██      ██      ██      ██      
+███████ ██    ██ ██      ██      █████   ███████ ███████ 
+     ██ ██    ██ ██      ██      ██           ██      ██ 
+███████  ██████   ██████  ██████ ███████ ███████ ███████                                                   
+"
+echo "data send succesffuly to the github "https://github.com/Kizaru1294989/Room_OS_Macro.git"
